83b4d8a9976e184670b1fab3173101a1d81bdbb3
Taran Mand <tsmand@yahoo.com>
2022-05-01 14:50:54 +1000

    initial commit

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { spacing, fontSizes } from '../utils/sizes';
+import { colors } from '../utils/colors';
+
+export const FocusHistory = ({ history }) => {
+  if (!history?.length) {
+    return <Text style={styles.title}>We haven't focussed on anything yet!</Text>
+  }
+
+  const renderItem = ({ item }) => <Text style={styles.item}>- {item}</Text>
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focussed on:</Text>
+      <FlatList data={history} renderItem={renderItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex: 1,
+  },
+  item: {
+    color: colors.white,
+    paddingTop: spacing.sm,
+    fontSize: fontSizes.md,
+  },
+  title: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: fontSizes.md,
+  },
+});
---
+++ src/features/Timer.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Vibration } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
+import { useKeepAwake } from 'expo-keep-awake';
+import { Countdown } from '../components/Countdown';
+import { RoundedButton } from '../components/RoundedButton';
+import { Timing } from './Timing';
+import { spacing } from '../utils/sizes';
+import { colors } from '../utils/colors';
+
+const ONE_SECOND_IN_MS = 1000;
+
+const PATTERN = [
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS
+];
+
+export const Timer = ({ focusSubject, clearSubject, onTimerEnd }) => {
+  useKeepAwake();
+  
+  const [isStarted, setIsStarted] = useState(false);
+  const [progress, setProgress] = useState(1);
+  const [minutes, setMinutes] = useState(0.1);
+
+  const onEnd = (reset) => {
+    Vibration.vibrate(PATTERN);
+    setIsStarted(false);
+    setProgress(1);
+    reset();
+    onTimerEnd(focusSubject);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.countdown}>
+        <Countdown 
+          minutes={minutes} 
+          isPaused={!isStarted} 
+          onEnd={onEnd} 
+          onProgress={setProgress} 
+        />
+        <View style={{ paddingTop: spacing.xxl}}>
+          <Text style={styles.title}>Focussing On:</Text>
+          <Text style={styles.task}>{focusSubject}</Text>
+        </View>
+      </View>
+      <View style={{ paddingTop: spacing.sm }}>
+        <ProgressBar 
+          progress={progress}
+          color={colors.ProgressBar} 
+          style={{ height: spacing.sm }} 
+        />
+      </View>
+      <View style={styles.timingWrapper}> 
+        <Timing onChangeTime={setMinutes} />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <RoundedButton title={isStarted ? 'pause' : 'start'} onPress={() => setIsStarted(prev => !prev)} />
+      </View>
+      <View style={styles.clearSubjectWrapper}> 
+        <RoundedButton size={50} title='-' onPress={clearSubject} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: 'green',
+  },
+  countdown: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor: 'yellow',
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'purple',
+  },
+  timingWrapper: {
+    flex: 0.1,
+    flexDirection: 'row',
+    paddingTop: spacing.xxl,
+  },
+  clearSubjectWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  title: {
+    color: colors.white,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  task: {
+    color: colors.white,
+    textAlign: 'center',
+  }
+});
